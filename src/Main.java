91aaef168fc2c81b62c426dcc1150c531edce5d7
Anna Strelkova <anyute.dm@gmail.com>
2023-04-01 16:33:31 +0300

    merge conflict resolved

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = new  int[] {1, 2, 300, 210};
+        long[] sales = new  long[] {1, 2, 300, 210};
         SalesManager salesManager = new SalesManager(sales);
-       int max = salesManager.max();
-       int min = salesManager.min();
-       int average = salesManager.average();
+       long max = salesManager.max();
+       long min = salesManager.min();
+       long average = salesManager.average();
        System.out.println(max);
        System.out.println(min);
        System.out.println(average);
